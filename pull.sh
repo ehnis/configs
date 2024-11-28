153a29a89bd43f0d680f823a33ca49e698df2a8e
ehnis <lublujisn78@gmail.com>
2024-11-28 20:02:23 +0300

    bugfix

--- pull.sh
+++ pull.sh
@@ -2,7 +2,7 @@
 if [ -f ./check ]; then
   git pull
   sudo rm -rf /etc/nixos/*
-  sudo cp ./machines ./modules ./stuff ./flake.lock ./flake.nix /etc/nixos/
+  sudo cp -r ./machines ./modules ./stuff ./flake.lock ./flake.nix /etc/nixos/
 else
   echo "change your working directory to dotfiles already"
 fi
