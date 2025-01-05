4ea21b15e62982660bfa42883e3227376eeeb622
ehnis <lublujisn78@gmail.com>
2025-01-05 03:10:57 +0300

    newrr

--- machines/nixos/configuration.nix
+++ machines/nixos/configuration.nix
@@ -101,7 +101,7 @@ in
 
   # Use mainline (or latest stable) kernel instead of LTS kernel
   #boot.kernelPackages = pkgs.linuxPackages_testing;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  #boot.kernelPackages = pkgs.linuxPackages_latest;
   #chaotic.scx.enable = true;
 
   # Enable SysRQ
